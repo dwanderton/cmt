7521b80855b75462b8ce205516640a6a9dad3336
golars497 <john_lyndonne_calzado497@hotmail.co.uk>
2017-07-09 23:13:35 +0100

    Integrated the "Multiple Post Thumbnails" plugin to easily add feature image controls to post
    So far only added the "Multiple Post Thumbnails" to the Industries admin
    form so that the user can add a Slider Image

--- functions.php
+++ functions.php
@@ -1,4 +1,15 @@
 <?php
+// Load external file to add support for MultiPostThumbnails. Allows you to set more than one "feature image" per post.
+require_once('/assets/multi-post-thumbnails.php');
+add_theme_support( 'post-thumbnails' );
+
+function debug_to_console( $data ) {
+    $output = $data;
+    if ( is_array( $output ) )
+        $output = implode( ',', $output);
+
+    echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+}
 
 /* hard code disable related posts */
 function jetpackme_remove_rp() {
@@ -40,59 +51,6 @@ function create_custom_post_types() {
 // add the create custom post types funtion to the intialize action list
 add_action( 'init', 'create_custom_post_types' );
 
-
-
-//create a single function to add all metaboxes to their respective posts
-function add_metaboxes() {
-	
-	//Callback to add media uploader control to industry meta box
-	function industry_meta_cb($post) {
-		global $content_width, $_wp_additional_image_sizes;
-		$image_id = get_post_meta( $post->ID, '_listing_image_id', true );
-		$old_content_width = $content_width;
-		$content_width = 254;
-		if ( $image_id && get_post( $image_id ) ) {
-			if ( ! isset( $_wp_additional_image_sizes['post-thumbnail'] ) ) {
-				$thumbnail_html = wp_get_attachment_image( $image_id, array( $content_width, $content_width ) );
-			} else {
-				$thumbnail_html = wp_get_attachment_image( $image_id, 'post-thumbnail' );
-			}
-			if ( ! empty( $thumbnail_html ) ) {
-				$content = $thumbnail_html;
-				$content .= '<p class="hide-if-no-js"><a href="javascript:;" id="remove_listing_image_button" >' . esc_html__( 'Remove listing image', 'text-domain' ) . '</a></p>';
-				$content .= '<input type="hidden" id="upload_listing_image" name="_listing_cover_image" value="' . esc_attr( $image_id ) . '" />';
-			}
-			$content_width = $old_content_width;
-		} else {
-			$content = '<img src="" style="width:' . esc_attr( $content_width ) . 'px;height:auto;border:0;display:none;" />';
-			$content .= '<p class="hide-if-no-js"><a title="' . esc_attr__( 'Set listing image', 'text-domain' ) . '" href="javascript:;" id="upload_listing_image_button" id="set-listing-image" data-uploader_title="' . esc_attr__( 'Choose an image', 'text-domain' ) . '" data-uploader_button_text="' . esc_attr__( 'Set listing image', 'text-domain' ) . '">' . esc_html__( 'Set listing image', 'text-domain' ) . '</a></p>';
-			$content .= '<input type="hidden" id="upload_listing_image" name="_listing_cover_image" value="" />';
-		}
-		echo $content;    
-	}
-	add_meta_box("industry-meta", "Slider Image", "industry_meta_cb", "industry", "side", "low");
-
-}
-add_action( 'add_meta_boxes', 'add_metaboxes' );
-
-function wpse_cpt_enqueue( $hook_suffix ){
-    $cpt = 'industry';
-
-    if( in_array($hook_suffix, array('post.php', 'post-new.php') ) ){
-        $screen = get_current_screen();
-
-        if( is_object( $screen ) && $cpt == $screen->post_type ){
-
-        	wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/assets/js/script.js' );
-            // Register, enqueue scripts and styles here
-
-        }
-    }
-}
-
-add_action( 'admin_enqueue_scripts', 'wpse_cpt_enqueue');
-
-
 //Create a single function to initialize all the different taxonomies
 function create_taxonomies() {
 
@@ -109,6 +67,26 @@ function create_taxonomies() {
 }
 
 add_action( 'init', 'create_taxonomies' );
-add_theme_support( 'post-thumbnails' );
+
+
+//function used to add feature images to posts
+function add_feature_image_controls_to_posts() {
+	// Add Custom image sizes
+	// Note: 'true' enables hard cropping so each image is exactly those dimensions and automatically cropped
+	add_image_size( 'feature-image', 960, 500, true ); 
+	add_image_size( 'medium-thumb', 300, 156, true );
+	add_image_size( 'small-thumb', 75, 75, true );
+
+	// Define additional "post thumbnails" for the Industry Custom Post Type. Relies on MultiPostThumbnails to work
+	if (class_exists('MultiPostThumbnails')) {
+	    new MultiPostThumbnails(array(
+	        'label' => 'Slider Image',
+	        'id' => 'industry-slider-image',
+	        'post_type' => 'industry'
+	        )
+	    );   
+	};
+}
+add_feature_image_controls_to_posts();
 
 ?>
