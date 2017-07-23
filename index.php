f8fc9e42934481c9e828703167a6e34a7879b421
David Anderton <dwanderton@gmail.com>
2017-07-23 13:40:43 -0400

    closes #4

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php get_header(); ?>
 
-  <div class="block block-inverse block-fill-height app-header"
-         style="background-image: url(<?php bloginfo('template_directory'); ?>/assets/img/startup-1.jpg);">
-
-    <div class="container py-4 fixed-top app-navbar">
+  <div id="carousel-example-generic-2" class="carousel carousel-light slide " data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic-2" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic-2" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic-2" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active" style="background-image: url(<?php bloginfo('template_directory'); ?>/assets/img/startup-1.jpg); min-height:600px;">
+<!--      <img class="d-block img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/startup-1.jpg" alt="First slide">-->
+      <div class="container py-4 fixed-top app-navbar">
       <nav class="navbar navbar-transparent navbar-padded navbar-toggleable-sm">
         <button
           class="navbar-toggler navbar-toggler-right hidden-md-up"
@@ -35,24 +41,129 @@
           </ul>
         </div>
       </nav>
-    </div>
-
-
-    <img class="app-graph"  src="<?php bloginfo('template_directory'); ?>/assets/img/startup-0.svg">
-
-    <div class="block-xs-bottom pb-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-10 col-lg-6">
-            <h1 class="block-titleData frequency">Translations for Shipping</h1>
-            <button class="btn btn-primary btn-lg">Request Your Quote</button>
+      </div>
+      
+      <div class="container py-4 fixed-bottom">
+        <div class="block-xs pb-5">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-10 col-lg-6">
+                <h1 class="block-titleData frequency">Translations for Shipping</h1>
+                <button class="btn btn-primary btn-lg">Request Your Quote</button>
+              </div>
+            </div>     
           </div>
-        </div>     
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item" style="background-image: url(<?php bloginfo('template_directory'); ?>/assets/img/startup-1.jpg); min-height:600px;">
+<!--      <img class="d-block img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/startup-1.jpg" alt="First slide">-->
+      <div class="container py-4 fixed-top app-navbar">
+      <nav class="navbar navbar-transparent navbar-padded navbar-toggleable-sm">
+        <button
+          class="navbar-toggler navbar-toggler-right hidden-md-up"
+          type="button"
+          data-target="#stage"
+          data-toggle="stage"
+          data-distance="-250">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <a class="navbar-brand mr-auto" href="">
+          <strong style="background: #fff; padding: 12px; border-radius: 4px; color: #28669F;">go</strong>
+        </a>
+
+        <div class="hidden-sm-down text-uppercase">
+          <ul class="navbar-nav">
+            <li class="nav-item px-1 ">
+              <a class="nav-link" href="#services">Services</a>
+            </li>
+            <li class="nav-item px-1 ">
+              <a class="nav-link" href="#languages">Languages</a>
+            </li>
+            <li class="nav-item px-1 ">
+              <a class="nav-link" href="#contact">Contact</a>
+            </li>
+            <li class="nav-item px-1 ">
+              <a class="nav-link" href="#requestAQuotation">Request a Quotation</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      </div>
+      
+      <div class="container py-4 fixed-bottom">
+        <div class="block-xs pb-5">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-10 col-lg-6">
+                <h1 class="block-titleData frequency">Translations for Shipping</h1>
+                <button class="btn btn-primary btn-lg">Request Your Quote</button>
+              </div>
+            </div>     
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item" style="background-image: url(<?php bloginfo('template_directory'); ?>/assets/img/startup-1.jpg); min-height:600px;">
+<!--      <img class="d-block img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/startup-1.jpg" alt="First slide">-->
+      <div class="container py-4 fixed-top app-navbar">
+      <nav class="navbar navbar-transparent navbar-padded navbar-toggleable-sm">
+        <button
+          class="navbar-toggler navbar-toggler-right hidden-md-up"
+          type="button"
+          data-target="#stage"
+          data-toggle="stage"
+          data-distance="-250">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <a class="navbar-brand mr-auto" href="">
+          <strong style="background: #fff; padding: 12px; border-radius: 4px; color: #28669F;">go</strong>
+        </a>
+
+        <div class="hidden-sm-down text-uppercase">
+          <ul class="navbar-nav">
+            <li class="nav-item px-1 ">
+              <a class="nav-link" href="#services">Services</a>
+            </li>
+            <li class="nav-item px-1 ">
+              <a class="nav-link" href="#languages">Languages</a>
+            </li>
+            <li class="nav-item px-1 ">
+              <a class="nav-link" href="#contact">Contact</a>
+            </li>
+            <li class="nav-item px-1 ">
+              <a class="nav-link" href="#requestAQuotation">Request a Quotation</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      </div>
+      
+      <div class="container py-4 fixed-bottom">
+        <div class="block-xs pb-5">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-10 col-lg-6">
+                <h1 class="block-titleData frequency">Translations for Shipping</h1>
+                <button class="btn btn-primary btn-lg">Request Your Quote</button>
+              </div>
+            </div>     
+          </div>
+        </div>
       </div>
     </div>
   </div>
-
-
+  <a class="carousel-control-prev" href="#carousel-example-generic-2" role="button" data-slide="prev">
+    <span class="icon icon-chevron-thin-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel-example-generic-2" role="button" data-slide="next">
+    <span class="icon icon-chevron-thin-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
   <div class="block" id="industries">
     <div class="container text-xs-center">
