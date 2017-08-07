30e231055be94b9c929b0450ea4a29ba71c55072
David Anderton <dwanderton@gmail.com>
2017-08-07 02:06:16 +0100

    closes #7

--- footer.php
+++ footer.php
@@ -2,39 +2,19 @@
 <div class="block block-inverse app-footer">
  <div class="container">
     <div class="row">
-      <div class="col-md-5 mb-5">
+      <div class="col-md-12 mb-5">
         <ul class="list-unstyled list-spaced">
-          <li class="mb-2"><h6 class="text-uppercase">About</h6></li>
-          <li class="text-muted">
-             We’ve been working on Go Analytics for the better part of a decade and are super proud of what we’ve created. If you’d like to learn more, or are interested in a job, contact us anytime at <a href="mailto: themes@getbootstrap.com">themes@getbootstrap.com</a>.
-          </li>
-        </ul>
-      </div>
-      <div class="col-md-2 offset-md-1 mb-5">
-        <ul class="list-unstyled list-spaced">
-          <li class="mb-2"><h6 class="text-uppercase">Product</h6></li>
-          <li class="text-muted">Features</li>
-          <li class="text-muted">Examples</li>
-          <li class="text-muted">Tour</li>
-          <li class="text-muted">Gallery</li>
-        </ul>
-      </div>
-      <div class="col-md-2 mb-5">
-        <ul class="list-unstyled list-spaced">
-          <li class="mb-2"><h6 class="text-uppercase">Apis</h6></li>
-          <li class="text-muted">Rich data</li>
-          <li class="text-muted">Simple data</li>
-          <li class="text-muted">Real time</li>
-          <li class="text-muted">Social</li>
-        </ul>
-      </div>
-      <div class="col-md-2 mb-5">
-        <ul class="list-unstyled list-spaced">
-          <li class="mb-2"><h6 class="text-uppercase">Legal</h6></li>
-          <li class="text-muted">Terms</li>
-          <li class="text-muted">Legal</li>
-          <li class="text-muted">Privacy</li>
-          <li class="text-muted">License</li>
+          <li class="mb-3"><h6 class="text-uppercase">Contact</h6></li>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6">
+                <li class="text-muted">Telephone: +44 (0) 7582 283 837</li>
+              </div>
+              <div class="col-md-6">
+                <li class="text-muted">Email: <a href="mailto: cmattiolo@hotmail.co.uk">cmattiolo@hotmail.co.uk</a></li>
+              </div>
+            </div>
+          </div>
         </ul>
       </div>
     </div>
